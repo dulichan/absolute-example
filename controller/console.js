038265293d61a60f17489fd0810f9ec317798a93
Chan <dulithaz@gmail.com>
2013-12-03 11:06:25 +0530

    first commit

---
+++ controller/console.js
@@ -0,0 +1,19 @@
+var addpost = function(appController){
+	return {
+		layout:'1-column-simple',
+		title:'Kick ass Blogger sample',
+		data:{
+			quote:"Millions of Expressions"
+		}
+	};
+}
+
+var help = function(appController){
+	return {
+		layout:'1-column-simple',
+		title:'Help',
+		data:{
+			quote:"Millions of Expressions"
+		}
+	};
+}
